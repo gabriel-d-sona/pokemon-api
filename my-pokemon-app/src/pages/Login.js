c0b66a6cb9b5386112bb28c61b34ab61a810a86d
gabriel-d-sona <gdsona@hotmail.com>
2024-06-03 20:30:16 -0300

    logica do login feita

--- my-pokemon-app/src/pages/Login.js
+++ my-pokemon-app/src/pages/Login.js
@@ -1,36 +1,73 @@
 import React from 'react';
+import { Navigate } from 'react-router-dom';
 
 class Login extends React.Component {
+    state = {
+        email: '',
+        senha: '',
+        btnDisable: true,
+        navigate: false,
+    };
+
+    // Função para validar o formato do e-mail
+    emailValido = (email) => {
+        const emailRegex = /^\w+([.-]?\w+)@\w+([.-]?\w+)(.\w{2,3})+$/;
+        return emailRegex.test(email);
+    }
+
+    // Função para lidar com mudanças nos inputs
+    onInputChange = (event) => {
+        const { target } = event;
+        const { value, name } = target;
+        this.setState(
+            (prevState) => {
+                const newState = { ...prevState, [name]: value };
+                const isFormValid = this.emailValido(newState.email) && newState.senha.length > 5;
+                return { ...newState, btnDisable: !isFormValid };
+            }
+        );
+    };
+
+    // Função para lidar com o clique do botão
+    btn = async () => {
+        this.setState({ navigate: true });
+    };
+
     render() {
+        const { email, senha, btnDisable, navigate } = this.state;
+        if (navigate) {
+            return <Navigate to="/menu" />;
+        }
         return (
             <div>
-                <p>Email:</p>
+                <label htmlFor="email">Email: </label>
                 <input
-                    id="name-input"
-                    data-testid="login-name-input"
+                    id="email-input"
+                    data-testid="login-email-input"
                     type="text"
-                    name="name"
-                    onChange={ this.inIputChange }
+                    name="email"
+                    value={email}
+                    onChange={this.onInputChange}
                 />
-                <p>Senha:</p>
+                <label htmlFor="senha">Senha: </label>
                 <input
                     id="senha-input"
                     data-testid="login-senha-input"
                     type="text"
                     name="senha"
-                    onChange={ this.inIputChange }
+                    value={senha}
+                    onChange={this.onInputChange}
                 />
-                <br/>
+                <br />
                 <button
                     id="btnLogin"
                     data-testid="login-btn"
-                    // disabled={ btnDisable }
-                    onClick={ this.btn }
+                    disabled={btnDisable}
+                    onClick={this.btn}
                 >
                     Entrar
                 </button>
             </div>
-
         );
     }
 }
