e3e91e0ee9517c81a14a4992d24a9b5db71a445a
gabriel-d-sona <gdsona@hotmail.com>
2024-06-04 22:20:47 -0300

    card para testar

--- my-pokemon-app/src/componentes/CardPokemon.js
+++ my-pokemon-app/src/componentes/CardPokemon.js
@@ -1,11 +1,24 @@
+import PropTypes from 'prop-types';
 import React from "react";
+// import { fetchPokemon } from "../functions/fetchFunctions";
 
 class CardPokemon extends React.Component {
     render() {
+        const { front_default, name, weight } = this.props;
         return (
-            <p>test</p>
-        )
+            <div>
+                <img src={ front_default } alt={ name } />
+                <h1>{ name }</h1>
+                <h2>Type:</h2>
+                <h2>Weight:</h2>
+                <h3>{ weight }</h3>
+            </div>
+        );
     }
 }
 
-export default CardPokemon;
+CardPokemon.propTypes = {
+    front_default: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    weight: PropTypes.number.isRequired,
+};
