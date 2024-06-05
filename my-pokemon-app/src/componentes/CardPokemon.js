f8aab6f116d45cfa212ea6d11b5206aaa1a5c89b
gabriel-d-sona <gdsona@hotmail.com>
2024-06-04 21:41:15 -0300

    iniciando criação do card

---
+++ my-pokemon-app/src/componentes/CardPokemon.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+class CardPokemon extends React.Component {
+    render() {
+        return (
+            <p>test</p>
+        )
+    }
+}
+
+export default CardPokemon;
